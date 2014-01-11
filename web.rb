8e27eb6e610b46aa50bacc092ad66aa3f0ac2f16
Joshua Kalpin <joshua.kalpin@gmail.com>
2014-01-11 14:24:23 -0500

    Testing out album creation

--- web.rb
+++ web.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
+require 'net/https'
+require 'net/http'
+require 'net/https'
 
 get '/' do
-  'Hello World'
+  imgur
+end
+
+
+def imgur
+  headers    = {
+      'Authorization' => 'Client-ID ' + ENV['IMGUR_API_KEY']
+  }
+  path = "/3/gallery/album/"
+  uri = URI("https://api.imgur.com#{path}")
+  request = Net::HTTP::Get.new(path, headers)
+
+  Net::HTTP.new(uri.host, uri.port).start {|http| http.request(request) }
 end
